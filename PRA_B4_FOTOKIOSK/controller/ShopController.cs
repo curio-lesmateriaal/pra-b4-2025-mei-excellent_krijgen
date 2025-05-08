fd59983b583ea7795172145560a7af4d44eb0d12
guzin-ka <D309123@edu.curio.nl>
2025-05-08 11:22:49 +0200

    Product afrekenen via formulier
    ik heb de formulier laten werken krijge je kan nu de bedrag zien aantla typen product soort kiezen en id invoeren

--- PRA_B4_FOTOKIOSK/controller/ShopController.cs
+++ PRA_B4_FOTOKIOSK/controller/ShopController.cs
@@ -1,50 +1,51 @@
 ﻿using PRA_B4_FOTOKIOSK.magie;
 using PRA_B4_FOTOKIOSK.models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PRA_B4_FOTOKIOSK.controller
 {
     public class ShopController
     {
-
         public static Home Window { get; set; }
 
         public void Start()
         {
-            // Stel de prijslijst in aan de rechter kant.
             ShopManager.SetShopPriceList("Prijzen:\nFoto 10x15: €2.55");
-
-            // Stel de bon in onderaan het scherm
             ShopManager.SetShopReceipt("Eindbedrag\n€");
-
-            // Vul de productlijst met producten
             ShopManager.Products.Add(new KioskProduct() { Name = "Foto 10x15" });
-            
-            // Update dropdown met producten
             ShopManager.UpdateDropDownProducts();
         }
 
-        // Wordt uitgevoerd wanneer er op de Toevoegen knop is geklikt
         public void AddButtonClick()
         {
-            
+            var fotoId = ShopManager.GetFotoId();
+            var amount = ShopManager.GetAmount();
+            var product = ShopManager.GetSelectedProduct();
+
+            if (fotoId == null || amount == null || product == null) return;
+
+            double prijsPerStuk = 2.55;
+            double totaal = prijsPerStuk * amount.Value;
+
+            ShopManager.AddShopReceipt($"\n{product.Name} x{amount} = €{totaal:F2}");
+
+            string fotoPath = $"foto_{fotoId}.jpg";
+            if (File.Exists(fotoPath))
+            {
+                File.Copy(fotoPath, $"bestelling_{fotoId}.jpg", true);
+            }
         }
 
-        // Wordt uitgevoerd wanneer er op de Resetten knop is geklikt
         public void ResetButtonClick()
         {
-
+            ShopManager.SetShopReceipt("Eindbedrag\n€");
         }
 
-        // Wordt uitgevoerd wanneer er op de Save knop is geklikt
         public void SaveButtonClick()
         {
+            string inhoud = ShopManager.GetShopReceipt();
+            File.WriteAllText("bon.txt", inhoud);
         }
-
     }
 }
