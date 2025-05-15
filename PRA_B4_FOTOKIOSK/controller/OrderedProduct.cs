0dab9d8ef52de691bc5eb8b8264feb9da91de69e
ArtemisStorm20 <D309906@edu.curio.nl>
2025-05-15 13:22:57 +0200

    Update OrderedProduct.cs

--- PRA_B4_FOTOKIOSK/controller/OrderedProduct.cs
+++ PRA_B4_FOTOKIOSK/controller/OrderedProduct.cs
@@ -8,5 +8,11 @@ namespace PRA_B4_FOTOKIOSK.controller
 {
     class OrderedProduct
     {
+            public int FotoNummer { get; set; }
+            public string ProductNaam { get; set; }
+            public int Aantal { get; set; }
+            public double TotaalPrijs { get; set; }
+        }
     }
-}
+
+
