0ef9095887e5671da4458ad512952665a9d2b724
guzin-ka <D309123@edu.curio.nl>
2025-06-02 11:23:34 +0200

    5 tot 30 min geldden fotos laten zien

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -1,9 +1,6 @@
 ﻿using PRA_B4_FOTOKIOSK.magie;
 using PRA_B4_FOTOKIOSK.models;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 namespace PRA_B4_FOTOKIOSK.controller
 {
@@ -16,37 +13,18 @@ namespace PRA_B4_FOTOKIOSK.controller
         {
             PicturesToDisplay.Clear();
 
-            int todayDayNumber = (int)DateTime.Now.DayOfWeek;
-            DateTime now = DateTime.Now;
+            string fotoMap = @"../../../fotos";
 
-            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
-            {
-                string folderName = Path.GetFileName(dir);
-                string[] folderParts = folderName.Split('_');
+            PicturesToDisplay = PictureManager.GetRecenteFotos(fotoMap);
 
-                if (folderParts.Length > 0 && int.TryParse(folderParts[0], out int folderDayNumber))
-                {
-                    if (folderDayNumber == todayDayNumber)
-                    {
-                        foreach (string file in Directory.GetFiles(dir))
-                        {
-                            DateTime created = File.GetCreationTime(file);
-
-                            TimeSpan verschil = now - created;
-                            if (verschil.TotalMinutes >= 5 && verschil.TotalMinutes <= 30)
-                            {
-                                PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
-                            }
-                        }
-                    }
-                }
-            }
+            PictureManager.UpdatePictures(PicturesToDisplay);
         }
 
         public void RefreshButtonClick()
         {
-            PicturesToDisplay.Clear();
             Start();
         }
     }
 }
+
+
--- PRA_B4_FOTOKIOSK/magie/PictureManager.cs
+++ PRA_B4_FOTOKIOSK/magie/PictureManager.cs
@@ -11,6 +11,61 @@ namespace PRA_B4_FOTOKIOSK.magie
     {
         public static Home Instance { get; set; }
 
+        public static List<KioskPhoto> GetRecenteFotos(string rootMap)
+        {
+            List<KioskPhoto> recenteFotos = new List<KioskPhoto>();
+
+            DateTime now = DateTime.Now;
+            DateTime minTime = now.AddMinutes(-30);
+            DateTime maxTime = now.AddMinutes(-2);
+
+            int todayDayNumber = (int)now.DayOfWeek;
+
+            foreach (string dagMap in Directory.GetDirectories(rootMap))
+            {
+                string folderName = Path.GetFileName(dagMap);
+                string[] parts = folderName.Split('_');
+
+                if (parts.Length > 0 && int.TryParse(parts[0], out int folderDayNumber))
+                {
+                    if (folderDayNumber == todayDayNumber)
+                    {
+                        foreach (string fotoPad in Directory.GetFiles(dagMap, "*.jpg"))
+                        {
+                            string fileName = Path.GetFileName(fotoPad);
+                            string[] fileParts = fileName.Split('_', '-');
+
+                            if (fileParts.Length >= 6)
+                            {
+                                try
+                                {
+                                    DateTime fotoTijd = new DateTime(
+                                        int.Parse(fileParts[0]),
+                                        int.Parse(fileParts[1]),
+                                        int.Parse(fileParts[2]),
+                                        int.Parse(fileParts[3]),
+                                        int.Parse(fileParts[4]),
+                                        int.Parse(fileParts[5].Split('.')[0])
+                                    );
+
+                                    if (fotoTijd >= minTime && fotoTijd <= maxTime)
+                                    {
+                                        recenteFotos.Add(new KioskPhoto { Source = fotoPad });
+                                    }
+                                }
+                                catch
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return recenteFotos;
+        }
+
         public static void UpdatePictures(List<KioskPhoto> picturesToDisplay)
         {
             if (Instance == null || Instance.spPictures == null)
@@ -27,13 +82,12 @@ namespace PRA_B4_FOTOKIOSK.magie
                 Image image = new Image
                 {
                     Source = bitmap,
-                    Width = 550,  // 1920 / 3.5
-                    Height = 308, // 1080 / 3.5
+                    Width = 550,
+                    Height = 308,
                     Margin = new System.Windows.Thickness(5)
                 };
 
                 Instance.spPictures.Children.Add(image);
-                Console.WriteLine(picture);
             }
         }
 
@@ -48,7 +102,7 @@ namespace PRA_B4_FOTOKIOSK.magie
                 img.StreamSource = stream;
                 img.CacheOption = BitmapCacheOption.OnLoad;
                 img.EndInit();
-                img.Freeze(); // voor performance en thread safety
+                img.Freeze();
 
                 return img;
             }
